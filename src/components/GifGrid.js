77b0d51f01406c3472bb754b420a55b902cf52f5
Fernando <fprios112@gmail.com>
2021-02-11 01:54:09 -0500

    llamando a las imagenes

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import GifGridItem from './GifGridItem';
 
 const GifGrid = ( {category} ) => {
+
+     // const [count, setcount] = useState(0);
+     const [images, setimages] = useState([])
+
+     useEffect( () => {
+          getGifs();
+     }, [] )
 
      const getGifs = async() => {
           const url = 'https://api.giphy.com/v1/gifs/search?q=pokeon&limit=10&api_key=C4JbEi66m6MiNtf98vfacDlrB4RgEeq6'
@@ -16,14 +24,32 @@ const GifGrid = ( {category} ) => {
           })
 
           console.log(gifs);
+          setimages(gifs);
      }
 
-     getGifs();
 
+
+     // return(
+     //      <>
+     //           <h3>{ category }</h3>
+     //           <ol>
+     //                {
+     //                     images.map( ({ id, title }) => (
+     //                          <li key={id}>{title}</li>
+     //                     ))
+     //                }
+     //           </ol>
+     //      </>
+     // )
 
      return(
           <>
                <h3>{ category }</h3>
+                    {
+                         images.map( img => (
+                              <GifGridItem key={img.id} {...img}/>
+                         ))
+                    }
           </>
      )
 }
